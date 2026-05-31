def97e332ec64c46e223dba878b57658209772fb
V1zRan <127090969+V1zRan@users.noreply.github.com>
2026-05-31 14:36:33 +1000

    Добавил создание задач через POST запрос

--- main.go
+++ main.go
@@ -6,12 +6,14 @@ import (
 	"net/http"
 )
 
+// Структура описывает задачу, которая существует в системе
 type Task struct {
 	ID    int    `json:"id"`
 	Title string `json"title"`
 	Done  bool   `json"done"`
 }
 
+// Структура для создания задачи пользователем
 type CreateTaskRequest struct {
 	Title string `json:"title"`
 }
@@ -34,14 +36,29 @@ var tasks = []Task{
 // Возвращает список задач в формате JSON
 func tasksHandler(w http.ResponseWriter, r *http.Request) {
 
+	// Получение списка задач
 	if r.Method == http.MethodGet {
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(tasks)
 		return
 	}
 
+	// Создание новой задачи
 	if r.Method == http.MethodPost {
-		// asd
+		// Структура для данных, пришедших от клиента
+		var req CreateTaskRequest
+		// Читаем JSON из тела запроса
+		json.NewDecoder(r.Body).Decode(&req)
+		// Создаем новую задачу
+		newTask := Task{
+			ID:    len(tasks) + 1,
+			Title: req.Title,
+			Done:  false,
+		}
+		tasks = append(tasks, newTask)
+		// Возвращаем созданную задачу клиенту
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(newTask)
 		return
 	}
 
